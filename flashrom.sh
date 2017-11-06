53cb19a88c880eb89c19864fbfe43d693d2aef64
0x1abin <270995079@qq.com>
2017-11-06 12:26:23 +0800

    first

---
+++ flashrom.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# . ${IDF_PATH}/add_path.sh
+ ~/Documents/Arduino/hardware/espressif/esp32/tools/esptool --chip esp32 --port "/dev/cu.SLAB_USBtoUART" --baud $((230400*4)) write_flash -fs 4MB 0x100000 "$1"
